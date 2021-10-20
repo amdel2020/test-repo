5d48d1c39ec622a9ca7a8bc3089280aaa7de1aa8
amdel2020 <amdel2020@gmail.com>
2021-10-20 21:40:35 +0530

    Add files via upload

---
+++ components/hero-post.tsx
@@ -0,0 +1,34 @@
+import Link from 'next/link'
+
+type Props = {
+  title: string
+  coverImage: string
+  date: string
+  excerpt: string
+  slug: string
+}
+
+const HeroPost = ({
+  title,
+  excerpt,
+  slug,
+}: Props) => {
+  return (
+    <section>
+      <div className="md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28">
+        <div>
+          <h3 className="mb-4 text-4xl lg:text-6xl leading-tight">
+            <Link as={`/posts/${slug}`} href="/posts/[slug]">
+              <a className="hover:underline">{title}</a>
+            </Link>
+          </h3>
+        </div>
+        <div>
+          <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default HeroPost
